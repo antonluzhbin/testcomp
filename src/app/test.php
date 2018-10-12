269920199a977b4f0aa73dd556818bbcd8726d3f
Anton Luzhbin <antonluzhbin@yandex.ru>
2018-10-12 13:20:58 +0400

    first commit

---
+++ src/app/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anton
+ * Date: 12.10.18
+ * Time: 12:41
+ */
+
+use MyComp\MyComp;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+MyComp::test();
+echo "\n";
